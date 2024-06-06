06d90469384f64551bb38989a71699cc02e9fe8f
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2024-06-06 19:41:58 -0300

    app running as HTTP server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hola Mundo desde estructura base...");
+const http = require("node:http");
+
+const PORT = 3000;
+
+const server = http.createServer( (request, response) =>{
+    response.writeHead(
+        200, {
+            'Content-type': 'text/plain'
+        });
+    
+    response.end('Hola Mundo');
+});
+
+server.listen(PORT, () => console.log("El servidor esta funcionadno en puerto: " + PORT));
