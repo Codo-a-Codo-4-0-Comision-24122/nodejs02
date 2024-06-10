5865537921611a6443ecc20244bb4348557d5598
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2024-06-10 19:12:53 -0300

    servidor de html

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const http = require("node:http");
+const fs = require('fs');
 
 const PORT = 3000;
 
-const server = http.createServer( (request, response) =>{
+const server = http.createServer((request, response) => {
+
+    const htmlFile = fs.readFileSync(__dirname + '/html/index.html');
+
     response.writeHead(
         200, {
-            'Content-type': 'text/plain'
-        });
-    
-    response.end('Hola Mundo');
+        'Content-type': 'text/html; charset=UTF-8'
+    });
+
+    response.end(htmlFile);
 });
 
 server.listen(PORT, () => console.log("El servidor esta funcionadno en puerto: " + PORT));
